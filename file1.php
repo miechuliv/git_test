5658fcb27d418db42e70bf762ae474ad7f7a5ceb
Mieszko Malawski <miechuliv@tlen.pl>
2014-11-03 22:56:41 +0100

    more frontedn

--- file1.php
+++ file1.php
@@ -5,10 +5,8 @@
 	echo 'chnages from dev1 on master with some extra';
 	echo 'some extra changes dev2 with fix from dev1';
 	
-	echo 'some more form dev1'
+	echo 'some more form dev1 and some frontedn'
 
-
-	echo 'some extra changes dev2.2';
 
 	echo 'some extra changes dev3 but also some from dev2.2';
 
